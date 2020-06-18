4e3b68d1e7b641f68815c03e154bdf2c882aad7b
Rahul Lokurte <rahullokurte@Rahuls-MacBook-Air.local>
2020-06-18 23:16:38 +0530

    Initial commit

---
+++ src/main/java/com/rahullokurte/updownload/UpDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.rahullokurte.updownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rahullokurte/updownload/UpDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahullokurte.updownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
